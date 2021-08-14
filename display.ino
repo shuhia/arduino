82b71f1831fcd63f0c89707e3f506976eaa7fe59
Alex On <shuhia.on@gmail.com>
2021-08-14 03:53:07 +0200

    feat(display): change boilerplate code

--- display.ino
+++ display.ino
@@ -1,64 +1,43 @@
 /*
-  LiquidCrystal Library - display() and noDisplay()
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD and uses the
- display() and noDisplay() functions to turn on and off
- the display.
-
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalDisplay
-
+Arduino 2x16 LCD - Detect Buttons
+modified on 18 Feb 2019
+by Saeed Hosseini @ Electropeak
+https://electropeak.com/learn/
 */
-
-// include the library code:
 #include <LiquidCrystal.h>
-
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
+//LCD pin to Arduino
+const int pin_RS = 8; 
+const int pin_EN = 9; 
+const int pin_d4 = 4; 
+const int pin_d5 = 5; 
+const int pin_d6 = 6; 
+const int pin_d7 = 7; 
+const int pin_BL = 10; 
+LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+ lcd.begin(16, 2);
+ lcd.setCursor(0,0);
+ lcd.print("Electropeak.com");
+ lcd.setCursor(0,1);
+ lcd.print("Press Key:");
 }
-
 void loop() {
-  // Turn off the display:
-  lcd.noDisplay();
-  delay(500);
-  // Turn on the display:
-  lcd.display();
-  delay(500);
-}
+ int x;
+ x = analogRead (0);
+ lcd.setCursor(10,1);
+ if (x < 60) {
+   lcd.print ("Right ");
+ }
+ else if (x < 200) {
+   lcd.print ("Up    ");
+ }
+ else if (x < 400){
+   lcd.print ("Down  ");
+ }
+ else if (x < 600){
+   lcd.print ("Left  ");
+ }
+ else if (x < 800){
+   LCD.print ("Select");
+ }
+} 
