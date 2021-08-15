9e1f36b097dd5e07b4d4b50dbbc12b161f4e1d11
Alex On <shuhia.on@gmail.com>
2021-08-15 05:50:18 +0200

    feat: Add menu and 2 digital pins

--- display.ino
+++ display.ino
@@ -6,46 +6,75 @@ https://electropeak.com/learn/
 */
 #include <LiquidCrystal.h>
 //LCD pin to Arduino
-const int pin_RS = 8; 
-const int pin_EN = 9; 
-const int pin_d4 = 4; 
-const int pin_d5 = 5; 
-const int pin_d6 = 6; 
-const int pin_d7 = 7; 
-const int pin_BL = 10; 
-LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
-void setup() {
- lcd.begin(16, 2);
- lcd.setCursor(0,0);
- lcd.print("Electropeak.com");
- lcd.setCursor(0,1);
- lcd.print("Press Key:");
+const int pin_RS = 8;
+const int pin_EN = 9;
+const int pin_d4 = 4;
+const int pin_d5 = 5;
+const int pin_d6 = 6;
+const int pin_d7 = 7;
+const int pin_BL = 10;
+
+// Available digital output
+const int pin_d2 = 2;
+const int pin_d3 = 3;
+
+LiquidCrystal lcd(pin_RS, pin_EN, pin_d4, pin_d5, pin_d6, pin_d7);
+
+String menuItems[6] = {"0", "1", "2", "3", "3", "4"};
+
+void initDisplay()
+{
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Electropeak.com");
+  lcd.setCursor(0, 1);
+  lcd.print("Select item: ");
+  lcd.print(menuItems[0]);
+}
+
+void initPins()
+{
+  pinMode(pin_d2, OUTPUT);
+  pinMode(pin_d3, OUTPUT);
+}
+
+void setup()
+{
+  initDisplay();
+  initPins();
 }
 
 // determine input
-void onInput(int x){
- lcd.setCursor(10,1);
- if (x < 60) {
-   handleRightButtonClick("Right ");
- }
- else if (x < 200) {
-   handleUpButtonClick("Up ");
- }
- else if (x < 400){
-   handleDownButtonClick("Down ");
- }
- else if (x < 600){
-   handleLeftButtonClick("Left ");
- }
- else if (x < 800){
-   handleSelectButtonClick("Select");
- }
-
+void onInput(int x)
+{
+  lcd.setCursor(10, 1);
+  if (x < 60)
+  {
+    handleRightButtonClick("Right ");
+  }
+  else if (x < 200)
+  {
+    handleUpButtonClick("Up ");
+  }
+  else if (x < 400)
+  {
+    handleDownButtonClick("Down ");
+  }
+  else if (x < 600)
+  {
+    handleLeftButtonClick("Left ");
+  }
+  else if (x < 800)
+  {
+    handleSelectButtonClick("Select");
+  }
 }
 
-// Add whitespace to text 
-String addWhiteSpace(String text, int length){
-  for(int i = 0; i<length; i++){
+// Add whitespace to text
+String addWhiteSpace(String text, int length)
+{
+  for (int i = 0; i < length; i++)
+  {
     text += " ";
   }
 
@@ -53,38 +82,40 @@ String addWhiteSpace(String text, int length){
 }
 
 // Input handlers
-void handleUpButtonClick(String args){
-     lcd.print("Top    ");
+void handleUpButtonClick(String args)
+{
+  lcd.print("Top");
 }
 
-void handleDownButtonClick(String args){
-     lcd.print("Bottom ");
+void handleDownButtonClick(String args)
+{
+  lcd.print("Bottom");
 }
 
-void handleLeftButtonClick(String args){
-     lcd.print("Left ");
+void handleLeftButtonClick(String args)
+{
+  lcd.print("Left");
 }
 
-void handleRightButtonClick(String args){
-     lcd.print("Right ");
+void handleRightButtonClick(String args)
+{
+  lcd.print("Right");
 }
 
-void handleSelectButtonClick(String args){
-     lcd.print("Select ");
+void handleSelectButtonClick(String args)
+{
+  lcd.print("Select");
 }
 
-void handleResetButtonClick(){
+void handleResetButtonClick()
+{
   lcd.print("Reset ");
 }
 
-
-
-
-void loop() {
- int x;
- // Check for input 
- x = analogRead (0);
+void loop()
+{
+  int x;
+  // Check for input
+  x = analogRead(0);
   onInput(x);
- 
-} 
-
+}
